bb90496bfceeb845b6996437a04e7e52aabeed2c
Anh Huy <xayda1207@gmail.com>
2022-12-07 14:13:01 +0700

    git fork

--- register.php
+++ register.php
@@ -46,23 +46,4 @@
 	</div>
 </body>
 
-<script>
-	$(document).ready(function () {
-    	$('#logo').addClass('animated fadeInDown');
-    	$("input:text:visible:first").focus();
-	});
-	$('#username').focus(function() {
-		$('label[for="username"]').addClass('selected');
-	});
-	$('#username').blur(function() {
-		$('label[for="username"]').removeClass('selected');
-	});
-	$('#password').focus(function() {
-		$('label[for="password"]').addClass('selected');
-	});
-	$('#password').blur(function() {
-		$('label[for="password"]').removeClass('selected');
-	});
-</script>
-
 </html>
